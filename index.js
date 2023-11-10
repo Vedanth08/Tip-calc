e41bc461c16fd67a08c5d894a5d0186d0a47e4db
Vednath08 <vedanth.jakkula@outlook.com>
2023-11-10 18:33:54 +0530

    Made the required changes

--- index.js
+++ index.js
@@ -1,29 +1,48 @@
 var billamountElement = document.getElementById("billamountField");
-var percentageElement = document.getElementById("percentageField");
-var tipamountElement = document.getElementById("tipamountField");
-var totalamountElement = document.getElementById("totalamountField");
-var consoleMessageField = document.getElementById("errormessageField");
 
+var percentageElement = document.getElementById("percentageField");
+
+var tipamountElement = document.getElementById("tipamountField");
+
+var totalamountElement = document.getElementById("totalamountField");
+
+var consoleMessageField = document.getElementById("errormessageField");
 
 function caluteButton(){
 
     let billAmount = parseInt(billamountElement.value);
-    let percentage = parseInt(percentageElement.value);
+
     let tipAmount = parseInt(tipamountElement.value);
+
     let totalAmount = parseInt(totalamountElement.value);
 
-    let tipCalculation = (billAmount)*(percentage) /100;
+
+    let tip_percentageValue = 3;
+
+    let tax_percentageValue = 5;
+
+
+    let tipCalculation = (billAmount)*(tip_percentageValue) /100;
+
     tipamountElement.value = tipCalculation;
 
-    let totalamountResult = Math.ceil( billAmount + tipCalculation );
+    let tax_Calucation = (billAmount)*(tax_percentageValue) /100;
+
+    let totalamountResult = Math.ceil( billAmount + tipCalculation + tax_Calucation);
+
     totalamountElement.value = totalamountResult;
 
-    if (billamountElement.value === ""){
-        errormessageField.textContent = "Please provide the amount";
+    
+    if( billamountElement.value ==="" ){
+
+        consoleMessageField.textContent = "Please provide the amount ";
+
     }
 
-    else if (percentageElement.value === ""){
-        errormessageField.textContent = "Please provide the amount";
+    else if (typeof(billamountElement.value) !== "number"){
+
+        consoleMessageField.textContent = "Please provide the  Vaild Input";
+
     }
 
 }
